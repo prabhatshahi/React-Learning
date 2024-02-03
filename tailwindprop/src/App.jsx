db8ef9b3e5deccc784743ce9fd01b0f5c9ff6de9
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-02-03 13:25:23 +0530

    tailinf

---
+++ tailwindprop/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './component/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let myObj ={
+    username: "hitesh",
+    age:21
+  }
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-5 rounded-xl'>Tailwind Test</h1>
+      <Card username="chaiaurcode" />
+      <Card username="prabhat"/>
+    </>
+  )
+}
+
+export default App
