917a05bc2eaca734f463dac29359f978796fc435
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-02-08 12:11:50 +0530

    login-logout-signup

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -3,7 +3,6 @@ import { Container, Logo, LogoutBtn } from "../index";
 import { Link } from "react-router-dom";
 import { UseSelector, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
